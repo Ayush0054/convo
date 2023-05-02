2e7b0726f6b12e504e9bb3a26ac9ae80c86be940
Ayush0054 <ayush5aprill@gmail.com>
2023-05-03 00:36:24 +0530

    added home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Home from "./pages/home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/pages/home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className=" flex justify-center">
+      <h1>Convo</h1>
+    </div>
+  );
+}
+
+export default Home;
