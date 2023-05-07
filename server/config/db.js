2e829e3f4494a50cd761c50675d5bfe36bd66ce0
Ayush0054 <ayush5aprill@gmail.com>
2023-05-07 10:22:03 +0530

    server fixed

--- server/config/db.js
+++ server/config/db.js
@@ -2,13 +2,16 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log(`Connected to MONGO DB ${conn.connection.host}`.cyan.underline);
+    const conn = await mongoose.connect(
+      "mongodb+srv://ayush0054:ayush0054@convo.eds8kdv.mongodb.net/test",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log(`Connected to MONGO DB ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error: ${error.message}`.red);
-    process.exit();
+    // process.exit();
   }
 };
--- server/server.js
+++ server/server.js
@@ -8,4 +8,6 @@ connectDB();
 app.get("/", (req, res) => {
   res.send("hello nigga");
 });
-app.listen(5000, () => console.log("server running"));
+app.listen(5000, function () {
+  console.log("Server is running on port " + 5000);
+});
