355204b0aa7bee57a45579f162e84fb4a86f1620
Ayush0054 <ayush5aprill@gmail.com>
2023-06-10 18:30:07 +0530

    some ui changes

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -6,34 +6,39 @@ function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const style = {
-    height: 600,
-    width: 600,
+    height: 500,
+    width: 500,
   };
   return (
     <div>
       <h1 className=" text-7xl text-center p-10 ">Login</h1>
-      <div className=" flex justify-evenly items-center bg-[#FFD7C0] ml-64 mr-64 h-auto rounded-lg shadow-2xl ">
-        <div>
-          <Lottie animationData={animation} style={style} className=" " />
-        </div>
-        <div className=" p-10 bg-orange-300 rounded-3xl shadow-2xl">
-          <form action="" className=" grid max-w-xs  items-center  ">
-            <h1 className="text-2xl">Email</h1>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className=" bg-white border p-2 m-3  rounded-3xl focus:outline-none"
-            />
-            <h1 className="text-2xl">Password</h1>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className=" bg-white border p-2 m-3  rounded-3xl focus:outline-none"
-            />
-            <button className=" bg-[#FD8D4E] m-5   rounded-lg "> Login</button>
-          </form>
+      <div className=" flex justify-center items-center">
+        <div className=" flex justify-between items-center bg-[#FFD7C0] ml-64 mr-64  h-auto rounded-lg shadow-2xl  max-w-4xl ">
+          <div>
+            <Lottie animationData={animation} style={style} className=" " />
+          </div>
+          <div className=" p-10 mr-5">
+            <form action="" className=" grid max-w-xs  items-center  ">
+              <h1 className="text-2xl">Email</h1>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className=" bg-white border p-2 m-3  rounded-3xl focus:outline-none"
+              />
+              <h1 className="text-2xl">Password</h1>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className=" bg-white border p-2 m-3  rounded-3xl focus:outline-none"
+              />
+              <button className=" bg-[#FD8D4E] m-5   rounded-lg  p-2 drop-shadow-xl">
+                {" "}
+                Login
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
--- client/src/pages/signUp.tsx
+++ client/src/pages/signUp.tsx
@@ -16,55 +16,60 @@ function SignIn() {
   return (
     <div>
       <h1 className=" text-7xl text-center p-10 ">SignUp</h1>
-      <div className=" flex justify-evenly items-center bg-[#FFD7C0] ml-64 mr-64 h-auto rounded-lg shadow-2xl ">
-        <div>
-          <Lottie animationData={animation} style={style} className=" " />
-        </div>
-        <div className=" p-8 m-8 bg-orange-300 rounded-3xl shadow-2xl">
-          <form action="" className=" grid max-w-xs  items-center  ">
-            <h1 className="text-2xl">Name</h1>
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className=" bg-white border p-2 m-3  rounded-3xl focus:outline-none"
-            />
-            <h1 className="text-2xl">Email</h1>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className=" bg-white border p-2 m-3  rounded-3xl focus:outline-none"
-            />
+      <div className=" flex justify-center items-center">
+        <div className=" flex justify-between items-center bg-[#FFD7C0] ml-64 mr-64 h-auto rounded-lg shadow-2xl  max-w-5xl  ">
+          <div>
+            <Lottie animationData={animation} style={style} className=" " />
+          </div>
+          <div className=" p-8 m-8">
+            <form action="" className=" grid max-w-xs  items-center  ">
+              <h1 className="text-2xl">Name</h1>
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className=" bg-white border p-2 m-3  rounded-3xl focus:outline-none"
+              />
+              <h1 className="text-2xl">Email</h1>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className=" bg-white border p-2 m-3  rounded-3xl focus:outline-none"
+              />
 
-            <h1 className="text-2xl">Password</h1>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className=" bg-white border p-2 m-3  rounded-3xl focus:outline-none"
-            />
-            <h1 className="text-2xl">Confirm Password</h1>
-            <input
-              type="password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              className=" bg-white border p-2 m-3  rounded-3xl focus:outline-none"
-            />
+              <h1 className="text-2xl">Password</h1>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className=" bg-white border p-2 m-3  rounded-3xl focus:outline-none"
+              />
+              <h1 className="text-2xl">Confirm Password</h1>
+              <input
+                type="password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                className=" bg-white border p-2 m-3  rounded-3xl focus:outline-none"
+              />
 
-            <h1 className="text-2xl">Picture</h1>
+              <h1 className="text-2xl">Picture</h1>
 
-            <input
-              id="file_input"
-              type="file"
-              accept="image/*"
-              value={img}
-              onChange={postImg}
-              className="block   focus:outline-none  p-2 m-3  text-sm   rounded-3xl cursor-pointer bg-gray-50 dark:text-gray-700  "
-            />
+              <input
+                id="file_input"
+                type="file"
+                accept="image/*"
+                value={img}
+                onChange={postImg}
+                className="block   focus:outline-none  p-2 m-3  text-sm   rounded-3xl cursor-pointer bg-gray-50 dark:text-gray-700  "
+              />
 
-            <button className=" bg-[#FD8D4E] m-5   rounded-lg "> SignUp</button>
-          </form>
+              <button className=" bg-[#FD8D4E] m-5   rounded-lg p-2 drop-shadow-xl">
+                {" "}
+                SignUp
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
