dd82f7e2e6ebe812246d450cd2a8a69c75cbed8a
Ayush0054 <ayush5aprill@gmail.com>
2023-06-11 19:56:04 +0530

    server changes

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
+import { router as userRoutes } from "./routes/userRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+// const userRoutes = require("./routes/userRoutes");
 
 const app = express();
-
+app.use(express.json());
 connectDB();
+console.log(connectDB());
+app.use("/api/user", userRoutes);
 
+app.use(notFound);
+app.use(errorHandler);
 app.get("/", (req, res) => {
   res.send("hello nigga");
 });
