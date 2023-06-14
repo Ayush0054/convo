99dc6ffd0ce6eeb766f6e19f62ab67e239ccdac1
Ayush0054 <ayush5aprill@gmail.com>
2023-06-15 01:31:30 +0530

    server changes

--- server/server.js
+++ server/server.js
@@ -1,20 +1,38 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
-import { router as userRoutes } from "./routes/userRoutes.js";
+import { router } from "./routes/userRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+import cors from "cors";
+import dotenv from "dotenv";
 // const userRoutes = require("./routes/userRoutes");
-
 const app = express();
 app.use(express.json());
 connectDB();
 console.log(connectDB());
-app.use("/api/user", userRoutes);
+
+app.use(cors());
+
+app.options("*", cors());
+
+app.use("/api/user", router);
 
 app.use(notFound);
 app.use(errorHandler);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/dist"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
 app.get("/", (req, res) => {
   res.send("hello nigga");
 });
+// const port = process.env.PORT || 5000;
+// app.listen(port, () =>
+//   console.log(`Listening to requests on port ${port}`.yellow)
+// );
 app.listen(5000, function () {
   console.log("Server is running on port " + 5000);
 });
