34ff4eb682d69e846189899557cc408074b9ca9e
Ayush0054 <ayush5aprill@gmail.com>
2023-06-15 22:38:03 +0530

    access chat and fetch chat created

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
+import { protect } from "../middleware/authMiddleware.js";
+import { accessChat, fetchChats } from "../controllers/chatController.js";
 
 const chatRoutes = express.Router();
 chatRoutes.route("/").post(protect, accessChat);
-// chatRoutes.route("/").get(protect,fetchChats);
+chatRoutes.route("/").get(protect, fetchChats);
 // chatRoutes.route("/group").post(protect,createGroupChat);
 // chatRoutes.route("/rename").put(protect,renameGroup);
 // chatRoutes.route("/groupremove").put(protect,removeFromGroup);
 // chatRoutes.route("/groupadd").put(protect,addToGroup);
+export { chatRoutes };
