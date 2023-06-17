397875cd587b6481855232dd5523c0ceb1359739
Ayush0054 <ayush5aprill@gmail.com>
2023-06-17 20:18:45 +0530

    routes for groupchat

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,12 +1,19 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
-import { accessChat, fetchChats } from "../controllers/chatController.js";
+import {
+  accessChat,
+  addToGroup,
+  createGroupChat,
+  fetchChats,
+  removeFromGroup,
+  renameGroup,
+} from "../controllers/chatController.js";
 
 const chatRoutes = express.Router();
 chatRoutes.route("/").post(protect, accessChat);
 chatRoutes.route("/").get(protect, fetchChats);
-// chatRoutes.route("/group").post(protect,createGroupChat);
-// chatRoutes.route("/rename").put(protect,renameGroup);
-// chatRoutes.route("/groupremove").put(protect,removeFromGroup);
-// chatRoutes.route("/groupadd").put(protect,addToGroup);
+chatRoutes.route("/group").post(protect, createGroupChat);
+chatRoutes.route("/rename").put(protect, renameGroup);
+chatRoutes.route("/groupadd").put(protect, addToGroup);
+chatRoutes.route("/groupremove").put(protect, removeFromGroup);
 export { chatRoutes };
