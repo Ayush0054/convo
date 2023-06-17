94459f7d5b2e56155f465741919c1b42325bf45b
Ayush0054 <ayush5aprill@gmail.com>
2023-06-18 01:46:57 +0530

    initial navbar

---
+++ client/src/components/navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="bg-[#FD8D4E] min-w-full max-h-xl flex justify-evenly p-3 shadow-xl">
+      <h1 className=" text-3xl">Hi Ayush</h1>
+      <img src="" alt="your pic" />
+      <h1 className=" text-3xl">logout</h1>
+      <h1 className=" text-3xl">create Group</h1>
+    </div>
+  );
+}
+
+export default Navbar;
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Navbar from "../components/navbar";
 
 function Home() {
   return (
     <div className=" flex justify-center  text-cyan-900 ">
-      <h1>Convo</h1>
+      <Navbar />
     </div>
   );
 }
