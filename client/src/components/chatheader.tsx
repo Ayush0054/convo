f0d991244675c133656a2f3683a4b0be801fdefe
Ayush0054 <ayush5aprill@gmail.com>
2023-06-20 00:09:06 +0530

    chat header

---
+++ client/src/components/chatheader.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Chatheader() {
+  return (
+    <div className=" text-black flex m-5 justify-between">
+      <h1 className="text-3xl ">Contacts</h1>
+      <div className=" flex ">
+        <div className=" shadow bg-[#fdf0ea] flex p-2 ">
+          <input type="text" className="  " />
+        </div>
+        <div className="shadow bg-[#FD8D4E]  m-2 ">
+          <h1>create</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Chatheader;
--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-
+import logo from "./logo.png";
 function Navbar() {
   return (
-    <div className="bg-[#FD8D4E] min-w-full max-h-xl flex justify-evenly p-3 shadow-xl">
-      <h1 className=" text-3xl">Hi Ayush</h1>
-      <img src="" alt="your pic" />
-      <h1 className=" text-3xl">logout</h1>
-      <h1 className=" text-3xl">create Group</h1>
+    <div className="bg-[#FD8D4E] min-w-full max-h-xl flex justify-between p-3 shadow-xl text-black">
+      <div className=" flex items-center  pr-2 rounded-xl shadow-2xl ">
+        <img src={logo} alt="" className="max-w-2 max-h-10" />
+        <h1 className=" text-2xl font-semibold">CONVO</h1>
+      </div>
+      <div className=" flex  gap-10 items-center">
+        <h1 className=" text-3xl font-medium">Ayush</h1>
+        <img src="" alt="your pic" />
+        <h1 className=" text-3xl font-medium">logout</h1>
+        <h1 className=" text-3xl font-medium">create Group</h1>
+      </div>
     </div>
   );
 }
