ad5d3ee7822813b230def418cb232080a1130f54
Ayush0054 <ayush5aprill@gmail.com>
2023-06-20 01:16:35 +0530

    logo added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,13 @@ import "./App.css";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import SignUp from "./pages/signUp";
+import Chat from "./pages/chat";
 
 function App() {
   return (
     <div className="">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Chat />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import Navbar from "../components/navbar";
 
 function Home() {
-  return (
-    <div className=" flex justify-center  text-cyan-900 ">
-      <Navbar />
-    </div>
-  );
+  return <div className=" "></div>;
 }
 
 export default Home;
