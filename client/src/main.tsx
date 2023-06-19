e81d2450106461bb55d63b1c230097891e743b7f
Ayush0054 <ayush5aprill@gmail.com>
2023-06-20 01:16:10 +0530

    provider and some css

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+// import Chatprovider from "./context/chatProvider";
+import { ChatProvider } from "./context/chatProvider";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ChatProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChatProvider>
   </React.StrictMode>
 );
