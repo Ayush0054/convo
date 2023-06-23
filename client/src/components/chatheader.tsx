326b386073ac7e23156e41cb0f78fa050a8b8031
Ayush0054 <ayush5aprill@gmail.com>
2023-06-24 01:48:03 +0530

    chat header update

--- client/src/components/chatheader.tsx
+++ client/src/components/chatheader.tsx
@@ -1,16 +1,22 @@
 import React from "react";
-
+import plus from "./plus.png";
 function Chatheader() {
   return (
     <div className=" text-black flex m-5 justify-between">
-      <h1 className="text-3xl ">Contacts</h1>
-      <div className=" flex ">
-        <div className=" shadow bg-[#fdf0ea] flex p-2 ">
-          <input type="text" className="  " />
-        </div>
-        <div className="shadow bg-[#FD8D4E] p-2 m-2 ">
-          <h1>create</h1>
-        </div>
+      <div className=" flex items-center gap-3">
+        <h1 className="text-5xl  ">Contacts</h1>
+        <button>
+          <img src={plus} alt="" />
+        </button>
+      </div>
+      <div className=" flex  items-center">
+        <input
+          type="text"
+          placeholder="Search/Add users to create group"
+          className="shadow bg-[#FBF3EF] flex p-2 outline-none w-[1000px]  "
+        />
+
+        <button className="shadow bg-[#FD8D4E] p-2 m-2 ">Create Group</button>
       </div>
     </div>
   );
