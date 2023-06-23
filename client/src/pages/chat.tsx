987b4f175e21989e34b1913613640e3965fb42ac
Ayush0054 <ayush5aprill@gmail.com>
2023-06-24 01:48:46 +0530

    login state

--- client/src/pages/chat.tsx
+++ client/src/pages/chat.tsx
@@ -1,23 +1,19 @@
 import React, { useEffect } from "react";
 import Navbar from "../components/navbar";
 import Chatheader from "../components/chatheader";
-import { useNavigate } from "react-router-dom";
+
+import ChatContact from "../components/chatContact";
+import ChatBox from "../components/chatBox";
 
 function Chat() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const user = JSON.parse(localStorage.getItem("userInfo") as string);
-
-    if (user) {
-      navigate("/chat");
-    }
-  }, [navigate]);
-
   return (
     <div>
       <Navbar />
       <Chatheader />
+      <div className="flex justify-between">
+        <ChatContact />
+        <ChatBox />
+      </div>
     </div>
   );
 }
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../components/navbar";
 
+import { useNavigate } from "react-router-dom";
+
 function Home() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("userInfo") as string);
+
+    if (user) {
+      navigate("/chat");
+    }
+  }, [navigate]);
   return <div className=" "></div>;
 }
 
