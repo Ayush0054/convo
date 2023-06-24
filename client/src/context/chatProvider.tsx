6c43ba630b256a6b22b8ea3e2cb0570f5c294941
Ayush0054 <ayush5aprill@gmail.com>
2023-06-24 22:35:47 +0530

    context updated for bool value

--- client/src/context/chatProvider.tsx
+++ client/src/context/chatProvider.tsx
@@ -10,11 +10,15 @@ import { useNavigate } from "react-router-dom";
 interface ChatContextinterface {
   user: string;
   setUser: React.Dispatch<React.SetStateAction<string>> | undefined;
+  search: boolean;
+  setSearch: React.Dispatch<React.SetStateAction<boolean>> | undefined;
 }
 
 const defaultState = {
   user: "",
   setUser: (user: "") => {},
+  search: false,
+  setSearch: (search) => {},
 } as ChatContextinterface;
 
 type ChildProps = {
@@ -23,10 +27,11 @@ type ChildProps = {
 
 const ChatContext = createContext(defaultState);
 // const  Provider  = ChatContext;
-
+const searchContext = createContext(defaultState);
 const ChatProvider = ({ children }: ChildProps) => {
   const [user, setUser] = useState<string>(defaultState.user);
-  const values = { user, setUser };
+  const [search, setSearch] = useState<boolean>(defaultState.search);
+  const values = { user, setUser, search, setSearch };
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -42,7 +47,7 @@ const ChatProvider = ({ children }: ChildProps) => {
 };
 
 const ChatState = () => {
-  useContext(ChatContext);
+  return useContext(ChatContext);
 };
 
 export { ChatProvider, ChatState };
