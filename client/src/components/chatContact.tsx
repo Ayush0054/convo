b7fc33c2df8cd18d517e9e074d3f921ec1beb062
Ayush0054 <ayush5aprill@gmail.com>
2023-06-24 21:31:36 +0530

    contact component

---
+++ client/src/components/chatContact.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Contact from "./contact";
+
+function ChatContact() {
+  return (
+    <div>
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+    </div>
+  );
+}
+
+export default ChatContact;
