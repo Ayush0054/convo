6c1358d06f0d003ae2e058f46c35caba0017a286
Ayush0054 <ayush5aprill@gmail.com>
2023-06-24 21:32:21 +0530

    logout added

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import logo from "./logo.png";
+import { ChatState } from "../context/chatProvider";
+import { useNavigate } from "react-router-dom";
 function Navbar() {
+  const navigate = useNavigate();
+  // const { user } = ChatState();
+  const lougout = () => {
+    localStorage.removeItem("userInfo");
+    navigate("/");
+  };
   return (
     <div className="bg-[#FD8D4E] min-w-full max-h-xl flex justify-between p-3 shadow-xl text-black">
       <div className=" flex items-center  pr-2 rounded-xl shadow-2xl ">
@@ -10,7 +18,9 @@ function Navbar() {
       <div className=" flex  gap-10 items-center">
         <h1 className=" text-3xl font-medium">Ayush</h1>
         <img src="" alt="your pic" />
-        <h1 className=" text-3xl font-medium">logout</h1>
+        <button onClick={lougout} className=" text-3xl font-medium">
+          logout
+        </button>
       </div>
     </div>
   );
