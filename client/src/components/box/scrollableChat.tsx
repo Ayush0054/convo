79b3ca42ad85b1b679301b1af86f611f4e1a5c39
Ayush0054 <ayush5aprill@gmail.com>
2023-07-16 20:07:25 +0530

    chatting feature

---
+++ client/src/components/box/scrollableChat.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import ScrollableFeed from "react-scrollable-feed";
+import { ChatState } from "../../context/chatProvider";
+import {
+  isLastMessage,
+  isSameSender,
+  isSameSenderMargin,
+  isSameUser,
+} from "../../config/chatLogics";
+import { Tooltip } from "react-tooltip";
+function ScrollableChat({ messages }: { messages: any }) {
+  const { user } = ChatState();
+  console.log(messages);
+  return (
+    <ScrollableFeed className="no-scrollbar">
+      {messages &&
+        messages.map((m: any, i: any) => (
+          <div className=" flex items-center gap-1" key={m._id}>
+            {(isSameSender(messages, m, i, user._id) ||
+              isLastMessage(messages, i, user._id)) && (
+              <div>
+                <a href="">
+                  <img
+                    // src={m.sender.picture}
+                    src="https://cdn.vox-cdn.com/thumbor/9TS9oKKcdKInFAHnWOOCE2qF8ng=/0x0:2864x1200/1400x1050/filters:focal(799x609:1257x1067):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/53591117/kongyell.0.jpg"
+                    alt=""
+                    className="my-anchor-element w-10 h-10 rounded-full "
+                  />
+                </a>
+                <Tooltip
+                  anchorSelect=".my-anchor-element"
+                  place="left"
+                  className=" from-neutral-950 "
+                >
+                  {m.sender.name}
+                </Tooltip>
+                {/* <h1>{m.sender.name}</h1> */}
+              </div>
+            )}
+            <span
+              className={` shadow ${
+                m.sender._id === user._id
+                  ? "bg-[#FFFDFC] rounded-br-none "
+                  : "bg-[#FD8D4E] rounded-bl-none"
+              } p-3 m-2 rounded-xl  `}
+              style={{
+                maxWidth: "75%",
+                marginLeft: isSameSenderMargin(messages, m, i, user._id),
+                marginTop: isSameUser(messages, m, i) ? 3 : 10,
+              }}
+            >
+              {m.content}
+            </span>
+          </div>
+        ))}
+    </ScrollableFeed>
+  );
+}
+
+export default ScrollableChat;
--- client/src/components/box/singleChat.tsx
+++ client/src/components/box/singleChat.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ChatState } from "../../context/chatProvider";
 import { getSender, getSenderFull } from "../../config/chatLogics";
 import ProfileModal from "../modal/profileModal";
 import UpdateGroupChatModal from "../modal/updateGroupChatModal";
-
+import Lottie from "lottie-react";
+import animation from "./chat-anime.json";
+import sndbtn from "./Vector.png";
+import axios from "axios";
+import ScrollableChat from "./scrollableChat";
 function SingleChat({
   fetchAgain,
   setFetchAgain,
@@ -11,32 +15,130 @@ function SingleChat({
   fetchAgain: any;
   setFetchAgain: any;
 }) {
+  const style = {
+    height: 600,
+    width: 600,
+  };
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [newMessage, setNewMessage] = useState();
   const { selectedChat, user, setSelectedChat } = ChatState();
+
+  const sendMessage = async (event: any) => {
+    if (event.key === "Enter" && newMessage) {
+      event.preventDefault();
+      try {
+        const config = {
+          headers: {
+            "Content-type": "application/json",
+            Authorization: `Bearer ${user.token}`,
+          },
+        };
+        setNewMessage("");
+        const { data } = await axios.post(
+          "http://localhost:5000/api/message",
+          {
+            content: newMessage,
+            chatId: selectedChat._id,
+          },
+          config
+        );
+        console.log(data);
+        setSelectedChat(selectedChat);
+        setMessages([...messages, data]);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const fetchMessages = async () => {
+    if (!selectedChat) return;
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      setLoading(true);
+      const { data } = await axios.get(
+        `http://localhost:5000/api/message/${selectedChat._id}`,
+        config
+      );
+      console.log(messages);
+
+      setMessages(data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchMessages();
+  }, [selectedChat]);
+
+  const typingHandler = (e: any) => {
+    setNewMessage(e.target.value);
+  };
   return (
     <div className=" flex justify-center items-center text-center">
       {selectedChat ? (
         <div>
-          {!selectedChat.isGroupChat ? (
-            <div className="flex justify-between  items-center p-3 border-b-2 border-gray-300">
-              <h1 className=" text-2xl ">
-                {getSender(user, selectedChat.users)}
-              </h1>
-              <ProfileModal user={getSenderFull(user, selectedChat.users)} />
+          <div className="shadow bg-[#FBF3EF]">
+            {!selectedChat.isGroupChat ? (
+              <div className="flex justify-between  items-center p-3 border-b-2 border-gray-300">
+                <h1 className=" text-2xl ">
+                  {getSender(user, selectedChat.users)}
+                </h1>
+                <ProfileModal user={getSenderFull(user, selectedChat.users)} />
+              </div>
+            ) : (
+              <div className="flex justify-between  items-center p-3 border-b-2 border-gray-300">
+                {selectedChat.chatName.toUpperCase()}
+                <UpdateGroupChatModal
+                  chat={selectedChat}
+                  setFetchAgain={setFetchAgain}
+                  fetchAgain={fetchAgain}
+                  fetchMessages={fetchMessages}
+                />
+              </div>
+            )}
+            <div className=" p-10 ">
+              {loading ? (
+                <div>
+                  <h1 className=" text-4xl">Loading</h1>
+                  <Lottie
+                    animationData={animation}
+                    style={style}
+                    className=" "
+                  />
+                </div>
+              ) : (
+                <div
+                  className=" flex object-contain   h-[600px]   "
+                  style={{ scrollbarWidth: "none", flexDirection: "column" }}
+                >
+                  <ScrollableChat messages={messages} />
+                </div>
+              )}
             </div>
-          ) : (
-            <div className="flex justify-between  items-center p-3 border-b-2 border-gray-300">
-              {selectedChat.chatName.toUpperCase()}
-              <UpdateGroupChatModal
-                chat={selectedChat}
-                setFetchAgain={setFetchAgain}
-                fetchAgain={fetchAgain}
-              />
-            </div>
-          )}
-          <div className=" pt-72 p-72 ">
-            {/* messages */}
-            hi
           </div>
+          <form
+            onKeyDown={sendMessage}
+            onClick={sendMessage}
+            className=" flex justify-between items-center gap-5"
+          >
+            <input
+              type="text"
+              className="shadow bg-[#fcefe9] mt-5 flex p-5 outline-none w-[1000px]  "
+              placeholder="Enter a Message"
+              onChange={typingHandler}
+              value={newMessage}
+            />
+            <button>
+              <img src={sndbtn} alt="" className=" mt-5 " />
+            </button>
+          </form>
         </div>
       ) : (
         <div className=" text-6xl pt-72 pb-72 text-center flex justify-center  ">
