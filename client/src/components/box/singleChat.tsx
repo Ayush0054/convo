3897a3664866acc1f18a09080d30a504646b3e49
Ayush0054 <ayush5aprill@gmail.com>
2023-07-17 00:49:57 +0530

    socket io frontend

--- client/src/components/box/singleChat.tsx
+++ client/src/components/box/singleChat.tsx
@@ -5,9 +5,14 @@ import ProfileModal from "../modal/profileModal";
 import UpdateGroupChatModal from "../modal/updateGroupChatModal";
 import Lottie from "lottie-react";
 import animation from "./chat-anime.json";
+import loadinganimation from "./loading.json";
 import sndbtn from "./Vector.png";
 import axios from "axios";
 import ScrollableChat from "./scrollableChat";
+import { io } from "socket.io-client";
+// @ts-ignore
+const ENDPOINT = "http://localhost:5000";
+var socket: any, selectedChatCompare: any;
 function SingleChat({
   fetchAgain,
   setFetchAgain,
@@ -16,17 +21,37 @@ function SingleChat({
   setFetchAgain: any;
 }) {
   const style = {
-    height: 600,
-    width: 600,
+    height: 500,
+    width: 500,
+  };
+  const style2 = {
+    height: 50,
+    width: 50,
   };
   const [messages, setMessages] = useState([]);
   const [loading, setLoading] = useState(false);
   const [newMessage, setNewMessage] = useState();
-  const { selectedChat, user, setSelectedChat } = ChatState();
-
+  const {
+    selectedChat,
+    user,
+    setSelectedChat,
+    setNotifications,
+    notifications,
+  } = ChatState();
+  const [socketConnected, setSocketConnected] = useState(false);
+  const [typing, setTyping] = useState(false);
+  const [istyping, setIstyping] = useState(false);
+  useEffect(() => {
+    socket = io(ENDPOINT);
+    socket.emit("setup", user);
+    socket.once("connected", () => setSocketConnected(true));
+    socket.on("typing", () => setIstyping(true));
+    socket.on("stop typing", () => setIstyping(false));
+  }, []);
   const sendMessage = async (event: any) => {
     if (event.key === "Enter" && newMessage) {
       event.preventDefault();
+      socket.emit("stop typing", selectedChat._id);
       try {
         const config = {
           headers: {
@@ -44,7 +69,8 @@ function SingleChat({
           config
         );
         console.log(data);
-        setSelectedChat(selectedChat);
+        socket.emit("new message", data);
+        // setSelectedChat(selectedChat);
         setMessages([...messages, data]);
       } catch (error) {
         console.log(error);
@@ -69,25 +95,60 @@ function SingleChat({
 
       setMessages(data);
       setLoading(false);
+      socket.emit("join room", selectedChat._id);
     } catch (error) {
       console.log(error);
     }
   };
   useEffect(() => {
     fetchMessages();
+    selectedChatCompare = selectedChat;
   }, [selectedChat]);
+
+  useEffect(() => {
+    socket.on("message recieved", (newMessageRecieved: any) => {
+      if (
+        !selectedChatCompare ||
+        selectedChatCompare._id !== newMessageRecieved.chat._id
+      ) {
+        if (!notifications.includes(newMessageRecieved)) {
+          setNotifications([...notifications, newMessageRecieved]);
+
+          console.log(notifications);
+          setFetchAgain(!fetchAgain);
+        }
+      } else {
+        setMessages([...messages, newMessageRecieved]);
+      }
+    });
+  });
 
   const typingHandler = (e: any) => {
     setNewMessage(e.target.value);
+    if (!socketConnected) return;
+    if (!typing) {
+      setTyping(true);
+      socket.emit("typing", selectedChat._id);
+    }
+    let lastTypingTime: any = new Date().getTime();
+    var timerLength = 3000;
+    setTimeout(() => {
+      var timNow = new Date().getTime();
+      var timeDiff = timNow - lastTypingTime;
+      if (timeDiff >= timerLength && typing) {
+        socket.emit("stop typing", selectedChat._id);
+        setTyping(false);
+      }
+    }, timerLength);
   };
   return (
-    <div className=" flex justify-center items-center text-center">
+    <div className=" flex justify-center items-center text-center ">
       {selectedChat ? (
         <div>
           <div className="shadow bg-[#FBF3EF]">
             {!selectedChat.isGroupChat ? (
               <div className="flex justify-between  items-center p-3 border-b-2 border-gray-300">
-                <h1 className=" text-2xl ">
+                <h1 className=" text-2xl uppercase font-semibold  ">
                   {getSender(user, selectedChat.users)}
                 </h1>
                 <ProfileModal user={getSenderFull(user, selectedChat.users)} />
@@ -105,23 +166,35 @@ function SingleChat({
             )}
             <div className=" p-10 ">
               {loading ? (
-                <div>
+                <div className=" grid items-center ">
                   <h1 className=" text-4xl">Loading</h1>
                   <Lottie
                     animationData={animation}
                     style={style}
-                    className=" "
+                    className=" flex items-center justify-center "
                   />
                 </div>
               ) : (
                 <div
-                  className=" flex object-contain   h-[600px]   "
+                  className=" flex object-contain   h-[470px]   "
                   style={{ scrollbarWidth: "none", flexDirection: "column" }}
                 >
                   <ScrollableChat messages={messages} />
                 </div>
               )}
             </div>
+            {istyping ? (
+              <div>
+                {" "}
+                <Lottie
+                  animationData={loadinganimation}
+                  style={style2}
+                  className=" "
+                />
+              </div>
+            ) : (
+              <div></div>
+            )}
           </div>
           <form
             onKeyDown={sendMessage}
