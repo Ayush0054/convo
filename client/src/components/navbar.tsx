52b50e39b206515e02ba43284fe69a60849222d8
Ayush0054 <ayush5aprill@gmail.com>
2023-08-01 01:01:44 +0530

    navbar ui fix

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -12,20 +12,20 @@ function Navbar() {
     navigate("/");
   };
   return (
-    <div className="bg-[#FD8D4E] min-w-full max-h-xl flex justify-between p-3 shadow-xl text-black">
-      <div className=" flex items-center  pr-2 rounded-xl shadow-2xl ">
-        <img src={logo} alt="" className="max-w-2 max-h-10" />
-        <h1 className=" text-2xl font-semibold">CONVO</h1>
+    <div className="bg-[#FD8D4E] min-w-full max-h-xl flex justify-between p-3 pb-5 pt-5 shadow-xl text-black uppercase">
+      <div className=" flex items-center  pr-2  ">
+        <img src={logo} alt="" className="max-w-2 max-h-14" />
+        <h1 className=" text-4xl font-semibold">CONVO</h1>
       </div>
       <div className=" flex  gap-10 items-center">
-        <h1 className=" text-3xl font-medium">{user.name}</h1>
+        <h1 className=" text-4xl font-medium">{user.name}</h1>
         <img
           src={user.picture}
           alt="your pic"
           className=" w-10 h-10 rounded-full "
         />
-        <button onClick={lougout} className=" text-3xl font-medium">
-          logout
+        <button onClick={lougout} className=" text-4xl font-medium">
+          Logout
         </button>
       </div>
     </div>
